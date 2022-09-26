39c3af2f09790a0f5ec8d39828e372bc73d2c8ab
ksanjiv05 <kumarsanjiv0005@gamil.com>
2022-09-26 16:40:05 +0530

    .env update

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 globalThis.__dirname = __dirname;
 const app: Express = express();
-const port = 443;
+const port = process.env.PORT|| 4000;
 app.use(cors());
 app.use(express.json({limit:"100mb"}))
 app.use(express.urlencoded({extended:true}))
